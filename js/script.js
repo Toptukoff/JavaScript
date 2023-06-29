bb5dae031983e5a59ecba1738fcb26f798a40365
AlexL <morozspb87@gmail.com>
2023-06-30 01:37:20 +0300

    опросник по фильмам 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-let art = 5;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+
+const pesonalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените фильм', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько оцените фильм', '');
+
+
+pesonalMovieDB.movies[a] = b;
+pesonalMovieDB.movies[c] = d;
+
+console.log(pesonalMovieDB);
